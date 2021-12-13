262881fe2237d42cdebd79292dcc7b4f680e1288
namuhuchutong <namuhuchutong@gmail.com>
2021-12-13 23:17:10 +0900

    java8 Optional

--- java/java8Practice/src/main/java/me/pepeprmill/java8Practice/App.java
+++ java/java8Practice/src/main/java/me/pepeprmill/java8Practice/App.java
@@ -1,8 +1,6 @@
 package me.pepeprmill.java8Practice;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -44,7 +42,7 @@ public class App {
 
         System.out.println("---두 수업 목록에 들어 있는 모든 수업 아이디 출력----");
         classEvents.stream()
-                .flatMap(l -> l.stream())
+                .flatMap(Collection::stream)
                 .forEach(oc -> System.out.println(oc.getId()));
 
         System.out.println("10부터 1씩 증가하는 무한 스트림 중에서 앞에 10개 빼고 최대 10개 까지");
@@ -58,10 +56,30 @@ public class App {
         System.out.println(test);
 
         System.out.println("스프링 수업 중에서 제목에 spring이 들어간 것들만 모아 list 생성");
-        List<String> spring = springClasses.stream().filter(oc -> oc.getTitle().contains("spring"))
-                .map(OnlineClass::getTitle)
+        List<String> spring = springClasses.stream().map(OnlineClass::getTitle)
+                .filter(title -> title.contains("spring"))
                 .collect(Collectors.toList());
         spring.forEach(System.out::println);
 
+
+        Optional<OnlineClass> springOpt = springClasses.stream()
+                .filter(oc -> oc.getTitle().startsWith("spring"))
+                .findAny();
+        boolean present = springOpt.isPresent();
+        System.out.println("present = " + present);
+
+        // isPresent를 사용하라고 IDE가 알려주는 중
+//        OnlineClass onlineClass = springOpt.get();
+//        System.out.println("onlineClass = " + onlineClass.getTitle());
+
+        // 개선된 방법
+        springOpt.ifPresent(oc -> System.out.println(oc.getTitle()));
+        OnlineClass onlineClass1 = springOpt.orElseGet(App::createNewClass);
+        System.out.println("onlineClass1 = " + onlineClass1.getTitle());
+    }
+
+    private static OnlineClass createNewClass() {
+        System.out.println("created");
+        return new OnlineClass(100, "New Class", false);
     }
 }
