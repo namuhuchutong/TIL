2d7db398bc2f1b28bd454eeddc2b1d3865eda5f9
namuhuchutong <namuhuchutong@gmail.com>
2022-01-31 21:03:29 +0900

    Cirtical section java

--- java/java8Practice/src/main/java/me/pepeprmill/os/CriticalSectionExample.java
+++ java/java8Practice/src/main/java/me/pepeprmill/os/CriticalSectionExample.java
@@ -1,2 +1,92 @@
-package me.pepeprmill.os;public class CriticalSectionExample {
+package me.pepeprmill.os;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.concurrent.Semaphore;
+
+public class CriticalSectionExample {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        testSemaphore();
+    }
+
+    private static void testSemaphore() throws InterruptedException {
+
+        final BankAccount bankAccount = new BankAccount(0, new Semaphore(1));
+        final Parent parent = new Parent(bankAccount);
+        final Child child = new Child(bankAccount);
+
+        parent.start();
+        child.start();
+        parent.join();
+        child.join();
+
+        System.out.println("\nBank Balance : " + bankAccount.getBalance());
+    }
+}
+
+@AllArgsConstructor
+@Data
+class BankAccount {
+
+    private int balance;
+    final private Semaphore sem;
+
+    void deposit(int amount) {
+        try {
+            sem.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        int temp = this.balance + amount;
+        System.out.print("+");
+        this.balance = temp;
+
+        sem.release();
+    }
+
+    void withdraw(int amount) {
+        try {
+            sem.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        int temp = this.balance - amount;
+        System.out.print("-");
+        this.balance = temp;
+
+        sem.release();
+    }
+}
+
+@AllArgsConstructor
+@Data
+class Parent extends Thread {
+
+    final private BankAccount bank;
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 50; i++) {
+            this.bank.deposit(100);
+        }
+    }
+}
+
+@AllArgsConstructor
+@Data
+class Child extends Thread {
+
+    final private BankAccount bank;
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 50; i++) {
+            this.bank.withdraw(100);
+        }
+    }
 }
