21a063f9ab15fb3fba9dcd6f2fdb2e046c474afa
namuhuchutong <namuhuchutong@gmail.com>
2022-02-21 21:37:17 +0900

    java bytecode test

--- java-playground/main/Main.java
+++ java-playground/main/Main.java
@@ -1,8 +1,18 @@
 class Main {
     
-    void add() {
+    public void test() {
+        Test t = new Test();
+        String name = "hello";
+        int big = 100000000;
+        t.add();
+    }
+}
+
+class Test {
+    
+    public int add() {
         int a = 1;
         int b = 2;
-        int c = a+b;
+        return a+b;
     }
 }
